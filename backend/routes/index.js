cc2668dc51433237616958f0bf490bf9d9322939
Somanshu Grover <somanshu63@gmail.com>
2024-06-01 19:00:06 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,29 +3,8 @@ var router = express.Router();
 const upload = require("../middlewares/upload");
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Welcome to vervegen project" });
 });
-router.post("/upload", upload.single("file"), async (req, res) => {
-  try {
-    const file = req.file;
-    console.log(file.path);
-    const fileDocument = new File({
-      filename: file.filename,
-      path: file.path,
-      mimeType: file.mimetype,
-      size: file.size,
-      uploadDate: new Date(),
-    });
-    const savedDocument = await fileDocument.save();
-    console.log(savedDocument);
-    res.status(200).send({
-      message: "File uploaded successfully",
-      file: savedDocument,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ error: error.message });
-  }
-});
+
 
 module.exports = router;
